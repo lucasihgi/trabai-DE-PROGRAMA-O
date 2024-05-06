6f3a58da2af1f392930c53547b6ddc6577f5bc31
lucas <128057845+lucasihgi@users.noreply.github.com>
2024-05-06 08:35:29 -0300

    Add files via upload

--- projetos do ano/main.js
+++ projetos do ano/main.js
@@ -2,6 +2,9 @@ const botoes = document.querySelectorAll(".botao");
 const texto= document.querySelectorAll(".aba-conteudo");
 const contadores= document.querySelectorAll(".contador");
 const tempoObjetivo1= new Date("2023-09-01T00:00:00");
+const tempoObjetivo2= new Date("2027-05-01T00:00:00");
+const tempoObjetivo3= new Date("2023-09-01T00:00:00");
+const tempoObjetivo4= new Date("2023-09-01T00:00:00");
 const tempoAtual1= new Date;
 //Declara quais elementos vão ser lidos. O script vai ler todos que tiverem a classe "botao".
 
@@ -21,3 +24,30 @@ for(let i=0;i <botoes.length;i++){
 }
 
 contadores[0].textContent= tempoObjetivo1 - tempoAtual1;
+
+
+
+contadores[0].textContent= cauculaTempo (tempoObjetivo1);
+contadores[1].textContent= cauculaTempo (tempoObjetivo1);
+contadores[2].textContent= cauculaTempo (tempoObjetivo1);
+contadores[3].textContent= cauculaTempo (tempoObjetivo1);
+
+
+function cauculaTempo (tempoObjetivo1){
+    let tempoAtual= new Date();
+    let tempoFinal= tempoObejetivo-tempoAtual;
+    let segundos= Math.floor(tempoFinal/1000);
+    let minutos= Math.floor(segundos/60);
+    let horas= Math.floor(minutos/60)
+    let dias= Math.floor(horas/24)
+    let anos= Math.floor(dias/365)
+    
+
+    segundos%=60;
+    minutos%=60;
+    horas%=24;
+
+    
+    return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
+
+}
