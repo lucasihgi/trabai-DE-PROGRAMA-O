08d5149d86fa4303159dd23742962751c93aabfa
lucas <128057845+lucasihgi@users.noreply.github.com>
2024-11-18 11:44:16 -0300

    Add files via upload

--- trabalho de senhas/main.js
+++ trabalho de senhas/main.js
@@ -1,5 +1,19 @@
 const numeroSenha=document.querySelector(".parametro-senha-texto");
-let tamanhoSenha=10;
+let tamanhoSenha=12;
 numeroSenha.textContent=tamanhoSenha;
-const botoes= document.querySelector(".parametro-senha-botoes"):
+const botoes= document.querySelectorAll(".parametro-senha-botao");
 botoes[0].onclick=diminuirTamanho;
+botoes[1].onclic=aumentartamanho
+function diminuirTamanho(){
+    if(tamanhoSenha>6){
+    tamanhoSenha=tamanhoSenha-1;
+    numeroSenha.textContent=tamanhoSenha;
+}
+}
+botoes[1].onclick=aumentartamanho;
+function aumentartamanho(){
+    if(tamanhoSenha<12){
+    tamanhoSenha=tamanhoSenha+1;
+    numeroSenha.textContent=tamanhoSenha;
+    }
+}
