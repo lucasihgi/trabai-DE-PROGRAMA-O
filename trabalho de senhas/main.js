223767d1f7dd44856d6d8a15363a35eff48146ec
lucas <128057845+lucasihgi@users.noreply.github.com>
2024-11-25 11:48:38 -0300

    Add files via upload

--- trabalho de senhas/main.js
+++ trabalho de senhas/main.js
@@ -2,6 +2,8 @@ const numeroSenha=document.querySelector(".parametro-senha-texto");
 let tamanhoSenha=12;
 numeroSenha.textContent=tamanhoSenha;
 const botoes= document.querySelectorAll(".parametro-senha-botao");
+const campoSenha= document.querySelector("#campo-senha");
+const letrasMaiusculas = "ABCDEFGHIJKLMNOPQRSTUVWYZÇ";
 botoes[0].onclick=diminuirTamanho;
 botoes[1].onclic=aumentartamanho
 function diminuirTamanho(){
@@ -11,9 +13,20 @@ function diminuirTamanho(){
 }
 }
 botoes[1].onclick=aumentartamanho;
+geraSenha();
 function aumentartamanho(){
     if(tamanhoSenha<12){
     tamanhoSenha=tamanhoSenha+1;
     numeroSenha.textContent=tamanhoSenha;
     }
+}
+function  geraSenha(){
+    let senha="";
+
+    for (let i=0; i<tamanhoSenha;i++){
+let numeroAleatorio=Math.random()*letrasMaiusculas.length
+numeroAleatorio=Math.floor(numeroAleatorio);
+senha=senha+letrasMaiusculas[numeroAleatorio];
+}
+campoSenha.value=senha;
 }
